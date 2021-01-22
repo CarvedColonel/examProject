b2e2f2118920531faefd22757f39cbfa30eab28c
milan <milanhennessy@gmail.com>
2021-01-22 09:57:21 -0500

    added aidan

--- src/main/java/mondesire/BattleSequence.java
+++ src/main/java/mondesire/BattleSequence.java
@@ -1,9 +1,18 @@
 package mondesire;
 /*
-Put header here
+Aidan Mason-Mondesire
+January 18th 2021
+The first battle sequence of six where you fight the zombie
+ */
 
+
+/*
+
+                                                        DISCLAIMER!!!
+    A LOT OF CODE GETS REPEATED BETWEEN SCENES SO SOME OF IT WON'T BE COMMENTED DUE TO IT BEING COMMENTED ELSEWHERE IN THE CODE
 
  */
+
 
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -22,6 +31,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Polyline;
 import javafx.scene.shape.Shape;
@@ -86,59 +97,96 @@ public class BattleSequence implements Initializable {
     @FXML
     private Button btnBack;
 
+    //imports for music
+    MediaPlayer battle;
+    MediaPlayer victory;
 
+    //importing zombie image
     Image zombie = new Image(getClass().getResource("/ZOMBIE.png").toString());
 
-
-
+    //the timer for the animation of the text and the delay before the enemy attacks
     Timeline UI = new Timeline(new KeyFrame(Duration.millis(5), ae -> ui()));
     Timeline pause = new Timeline(new KeyFrame(Duration.millis(1000), ae -> pauseVoid()));
 
+    //player and enemy health
     int health = 100;
     int zombieHealth = 50;
 
+    //whether you are attempting to fight the enemy or find a cheeky way out of the fight
     boolean fight;
     boolean bless;
 
+    //variables for your abilities damage
     int smiteDmg;
     int spearDmg;
     int pray = 2;
 
+    //timer for seconds
     int pauseTimer = 0;
 
+    //enemy's attack damage
     int zombieAttack;
 
 
     @FXML
     void clickBack(ActionEvent event) throws IOException {
+        //puts you back to the main menu and stops the music
         MainApp.setRoot("Gameplay", "Priest's Conquest");
+        if (MainApp.sound == true) {
+            victory.stop();
+        }
     }
 
-    void die(){
-        if(health <= 0){
+    void die() {
+        //if your health goes below zero, set the health bar back to empty, turn off all the labels, allow the return to map, and stop the music
+        if (health <= 0) {
+
             health = 0;
-            lblPlayerHealth.setText(""+health);
-            toggleOptions(false,false);
+            lblPlayerHealth.setText("" + health);
+            toggleOptions(false, false);
             AnimateText(lblMessage, "You Have Died! Returning to checkpoint.");
             btnBack.setVisible(true);
+
+            if (MainApp.sound == true) {
+                battle.stop();
+            }
+
+        }
+    }
+
+    void win() {
+        //if you win, set the enemy's health to zero, add one to wincount so we know player's progress, toggle the labels, allow them to return to map, add gold, and stop the music
+        zombieHealth = 0;
+        lblEnemyHealth.setText("" + zombieHealth);
+        AnimateText(lblMessage, "You defeated the Zombie!");
+        MainApp.winCount = 1;
+        toggleOptions(false, false);
+        btnBack.setVisible(true);
+        MainApp.gold = MainApp.gold + 10;
+
+        if (MainApp.sound == true) {
+            battle.stop();
+            victory.play();
         }
     }
 
     @FXML
     void clickFight(MouseEvent event) {
+        //set the variables so that fight is the stance they're taking and bring up the moves
         fight = true;
         bless = false;
         lblMove1.setVisible(true);
         lblMove2.setVisible(true);
         lblMove3.setVisible(true);
         lblMove1.setText("SMITE");//base dmg: 9-12
-        lblMove2.setText("HOLY SPEAR");//base dmg: 6-15
-        lblMove3.setText("PRAY("+pray+"/2)");//heals 25 health (Can be used twice per fight)
+        lblMove2.setText("HOLY SPEAR");//base dmg: 6-20
+        lblMove3.setText("PRAY(" + pray + "/2)");//heals 25 health (Can be used twice per fight)
     }
 
 
     @FXML
     void clickBless(MouseEvent event) {
+        //set the variables so that bless is the stance they're taking and bring up the moves
         bless = true;
         fight = false;
         lblMove1.setVisible(true);
@@ -150,6 +198,7 @@ public class BattleSequence implements Initializable {
     }
 
     void pauseVoid() {
+        //when the timer is running, count up by 1. When it hits 3 seconds let the enemy make their move and reset the timer.
         pauseTimer++;
         if (pauseTimer == 3) {
             zombieAttack();
@@ -159,22 +208,29 @@ public class BattleSequence implements Initializable {
     }
 
     void zombieAttack() {
+        //roll rng to see which attack the enemy uses, then use the rng of said move to determine how much damage it does. Toggle the players move options and check if they've died
         zombieAttack = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+
         if (zombieAttack == 1) {
+
             AnimateText(lblMessage, "The Zombie used Scratch!");
             int scratch = ThreadLocalRandom.current().nextInt(10, 15 + 1);
             health = health - scratch;
             lblPlayerHealth.setText("" + health);
             toggleOptions(true, false);
             die();
+
         } else if (zombieAttack == 2) {
+
             AnimateText(lblMessage, "The Zombie used Bite!");
             int bite = ThreadLocalRandom.current().nextInt(15, 20 + 1);
             health = health - bite;
             lblPlayerHealth.setText("" + health);
             toggleOptions(true, false);
             die();
+
         } else if (zombieAttack == 3) {
+
             AnimateText(lblMessage, "The Zombie used Lunge!");
             int lunge = ThreadLocalRandom.current().nextInt(5, 20 + 1);
             health = health - lunge;
@@ -185,7 +241,7 @@ public class BattleSequence implements Initializable {
     }
 
     void toggleOptions(boolean tf, boolean all) {
-
+        //toggles the options based on what you set the variables. tf will set the two main options, and all changes the moves that come up after clicking the two main options
         lblMove1.setVisible(all);
         lblMove2.setVisible(all);
         lblMove3.setVisible(all);
@@ -197,27 +253,29 @@ public class BattleSequence implements Initializable {
 
     @FXML
     void clickMove1(MouseEvent event) {
-
+        //if they chose to fight, then use smite move that does 10-14 damage, run the animations, and toggle the UI. if they choose bless nothing (zombies dont understand jokes)
+        //if the zombie dies run win code, if not then subtract the health and play the zombies move
         if (fight == true) {
+
             smiteDmg = ThreadLocalRandom.current().nextInt(10, 14 + 1);
             zombieHealth = zombieHealth - smiteDmg;
+
             if (zombieHealth <= 0) {
-                zombieHealth = 0;
-                lblEnemyHealth.setText("" + zombieHealth);
-                AnimateText(lblMessage, "You defeated the Zombie!");
-                toggleOptions(false, false);
-                btnBack.setVisible(true);
-                MainApp.winCount = 1;
-                MainApp.gold = MainApp.gold + 10;
+
+                win();
+
             } else {
+
                 lblEnemyHealth.setText("" + zombieHealth);
                 AnimateText(lblMessage, "You did " + smiteDmg + " damage to the Zombie!");
                 pause.play();
+
                 toggleOptions(false, false);
 
             }
 
         } else if (bless = true) {
+
             AnimateText(lblMessage, "The Zombie didn't understand the joke...");
             toggleOptions(true, false);
         }
@@ -227,22 +285,23 @@ public class BattleSequence implements Initializable {
     void clickMove2(MouseEvent event) {
 //if they chose to fight, then use the holy spear move that does 6-20 damage, run the animations, and toggle the UI. if they choose bless nothing (zombies can't be exorcised)
         if (fight == true) {
+
             spearDmg = ThreadLocalRandom.current().nextInt(6, 20 + 1);
             zombieHealth = zombieHealth - spearDmg;
-            if(zombieHealth <= 0) {
-                zombieHealth = 0;
-                lblEnemyHealth.setText("" + zombieHealth);
-                AnimateText(lblMessage, "You defeated the Zombie!");
-                toggleOptions(false, false);
-                btnBack.setVisible(true);
-                MainApp.winCount = 1;
-                MainApp.gold = MainApp.gold + 10;
-            }else{
+
+            if (zombieHealth <= 0) {
+
+                win();
+
+            } else {
+
                 lblEnemyHealth.setText("" + zombieHealth);
                 AnimateText(lblMessage, "You did " + spearDmg + " damage to the Zombie!");
                 pause.play();
                 toggleOptions(false, false);
+
             }
+
 
         } else if (bless = true) {
             AnimateText(lblMessage, "Zombies can't be exorcised...");
@@ -258,30 +317,31 @@ public class BattleSequence implements Initializable {
             if ((pray > 0) && ((health < 100))) {
                 pray--;
                 health = health + 25;
-                if(health > 100){
+
+                if (health > 100) {
                     health = 100;
                 }
+
                 lblPlayerHealth.setText("" + health);
                 AnimateText(lblMessage, "You healed 25 health!");
                 pause.play();
+
                 toggleOptions(false, false);
+
             } else {
                 lblMessage.setText("Can't perform that action");
             }
 
         } else if (bless = true) {
-            if(lblMove3.getText() == "[LOCKED]"){
+            if (lblMove3.getText() == "[LOCKED]") {
+//do nothing
+            } else {
+                if (zombieHealth < 0) {
 
-            }else{
-                if(zombieHealth < 0){
-                    zombieHealth = 0;
-                    lblEnemyHealth.setText("" + zombieHealth);
-                    AnimateText(lblMessage, "You defeated the Zombie!");
-                    MainApp.winCount = 1;
-                    toggleOptions(false, false);
-                    btnBack.setVisible(true);
-                    MainApp.gold = MainApp.gold + 10;
-                }else{
+                    win();
+
+                } else {
+
                     zombieHealth = zombieHealth - 50;
                     lblEnemyHealth.setText("" + zombieHealth);
                     AnimateText(lblMessage, "You did 50 damage to the Zombie!");
@@ -324,17 +384,26 @@ public class BattleSequence implements Initializable {
         //timer for the Interface stuff
         //these are code for hovering over the options, when you hover over an option text with a description will pop up
         if (fight == true) {
+
             changePrompt(true, false);
+
             lblInfo.textProperty().bind(Bindings.when(lblMove1.hoverProperty()).then("10-14dmg").otherwise(""));
             lblInfo2.textProperty().bind(Bindings.when(lblMove2.hoverProperty()).then("6-20dmg").otherwise(""));
             lblInfo3.textProperty().bind(Bindings.when(lblMove3.hoverProperty()).then("Heal 25 Health " + "\n" + " (Uses Charge)").otherwise(""));
+
         } else if (fight == false) {
+
             changePrompt(false, true);
+
             lblBInfo.textProperty().bind(Bindings.when(lblMove1.hoverProperty()).then("Tell a joke, maybe they'll like it").otherwise(""));
             lblBInfo2.textProperty().bind(Bindings.when(lblMove2.hoverProperty()).then("Some enemies may be susceptible to an exorcism").otherwise(""));
+
             if (MainApp.holyWater == true) {
+
                 lblBInfo3.textProperty().bind(Bindings.when(lblMove3.hoverProperty()).then("Does 50 Damage flat to your enemy (1 USE)").otherwise(""));
+
             } else {
+
                 lblBInfo3.textProperty().bind(Bindings.when(lblMove3.hoverProperty()).then("[LOCKED]").otherwise(""));
 
             }
@@ -353,10 +422,24 @@ public class BattleSequence implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        //declaring what files battle and victory music are set to
+        battle = new MediaPlayer((new Media(getClass().getResource("/BattleMusic.mp3").toString())));
+        victory = new MediaPlayer((new Media(getClass().getResource("/WinMusic.mp3").toString())));
+        //sets the music volume
+        battle.setVolume(25);
+        victory.setVolume(25);
+        //set how many times the song replays
+        battle.setCycleCount(MediaPlayer.INDEFINITE);
+        victory.setCycleCount(MediaPlayer.INDEFINITE);
+        //if sound is on then play the music
+        if (MainApp.sound == true) {
+            battle.play();
+        }
+        //set the clycle count of the timers
         UI.setCycleCount(Timeline.INDEFINITE);
         pause.setCycleCount(Timeline.INDEFINITE);
         UI.play();
-
+        //set the image to be the zombie, set the height,width, and position, and show the prompt text
         if (MainApp.battleStage == 1) {
             imgEnemy.setImage(zombie);
             imageSize(238, 146, 1011, 305);
