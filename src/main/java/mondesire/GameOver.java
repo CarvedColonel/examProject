e52531906c319eeb363df01d9661c936c43f2898
milan <milanhennessy@gmail.com>
2021-01-22 10:03:00 -0500

    added aidan

--- src/main/java/mondesire/GameOver.java
+++ src/main/java/mondesire/GameOver.java
@@ -5,26 +5,49 @@ Put header here
 
  */
 
+import javafx.animation.Animation;
+import javafx.animation.Transition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameOver implements Initializable {
-    
+
     @FXML
-    private Label lblOut;
-    
+    private Label lblMessage;
+
     @FXML
-    private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hello World!");
+    private void clickExit(MouseEvent event){
+        System.exit(0);
     }
-    
+
+    public void AnimateText(Label lbl, String descImp) {
+        String content = descImp;
+        final Animation animation = new Transition() {
+            {
+                setCycleDuration(Duration.millis(15000));
+            }
+
+            protected void interpolate(double frac) {
+                final int length = content.length();
+                final int n = Math.round(length * (float) frac);
+                lbl.setText(content.substring(0, n));
+            }
+        };
+        animation.play();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+        AnimateText(lblMessage,"Thank you for saving us" + "\n" + "Sir Priest! We wish you" + "\n" + "safe travels!" + "\n" + "\n" + "Thank you for playing!"
+                + "\n" + "Game made by:" + "\n" + "Aidan Mason-Mondesire" + "\n" + "and Milan Hennessy.");
+
+    }
 }
